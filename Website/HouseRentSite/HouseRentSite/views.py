41eb9a1f013b298cfb88c574a9ab379f3df45ab3
Anurodh Mohapatra <anurodhmohapatra@gmail.com>
2021-01-20 21:04:09 +0530

    Website

--- Website/HouseRentSite/HouseRentSite/views.py
+++ Website/HouseRentSite/HouseRentSite/views.py
@@ -7,24 +7,23 @@ import pandas as pd
 model = pickle.load(open('model.pkl', 'rb'))
 map_add = json.load(open('map.json', 'r'))
 
-def predict(Address,Size):
-    X = pd.DataFrame([[Address,Size]],columns=['Address','Size(Acres)'])
+
+def predict(Address, Size):
+    X = pd.DataFrame([[Address, Size]], columns=['Address', 'Size(Acres)'])
     X['Address'] = X['Address'].map(map_add)
     prediction = model.predict(X)
     return int(prediction)
 
+
 def home(request):
     params = {'add': list(map_add.keys())}
-    return render(request,'index.html',params)
+    return render(request, 'index.html', params)
+
 
 def index(request):
-    address = str(request.GET.get('address','others'))
-    size = int(request.GET.get('size','500'))
-    try:
-      pred = predict(address,size)
-    except:
-      pred = "Please make sure Property Size in numeric and above 500!!!"
-    button = request.GET.get('button',False)
-    params = {'add':list(map_add.keys()),
-              'pred':pred}
-    return render(request,'index.html',params)
+    address = str(request.GET.get('address', 'others'))
+    size = int(request.GET.get('size', '500'))
+    pred = predict(address, size)
+    params = {'add': list(map_add.keys()),
+              'pred': f"Monthly Rent for {size} sqft in {address} is Rs {pred}"}
+    return render(request, 'index.html', params)
